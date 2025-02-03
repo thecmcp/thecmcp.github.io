5a1cf765615339b92735852bcb222752ca11e99f
yonmaor1 <yonmaor@cmu.edu>
2025-02-03 12:11:29 -0500

    added scroll button to #home

--- static/scrollEffects.js
+++ static/scrollEffects.js
@@ -1,14 +1,23 @@
 function toggleNavBar() {
     let orbitingLogo = document.getElementById('orbiting-logo');
     let navSide = document.getElementById('nav-side');
+    let scrollDown = document.getElementById('scroll-down');
     if (window.scrollY > 100) {
         orbitingLogo.classList.add('shrink');
         navSide.style.transform = 'translateX(0%)';
+        scrollDown.style.display = 'none';
     } else {
         orbitingLogo.classList.remove('shrink');
         navSide.style.transform = 'translateX(-100%)';
+        scrollDown.style.display = 'block';
     }
 }
 
-document.addEventListener('DOMContentLoaded', createOrbit);
+document.addEventListener('DOMContentLoaded', () => {
+    createOrbit();
+    const scrollDown = document.getElementById('scroll-down');
+    scrollDown.addEventListener('click', () => {
+        document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    });
+});
 window.addEventListener('scroll', toggleNavBar);
